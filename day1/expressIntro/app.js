9a226516438cf9758c81f6b965b4e58645bd6726
Phil Chaplin <pchapl1@gmail.com>
2022-09-07 13:41:25 -0600

    second commit

--- day1/expressIntro/app.js
+++ day1/expressIntro/app.js
@@ -10,7 +10,7 @@ const port = 3000
 
 let globalFirstName = null
 let globalLastName = null
-const favoriteMovieList = ['The Matrix', 'The Other Guys', 'The Shawshank Redemption']
+
 
 app.get('/', (req, res) => {
     const myName = 'Phil Chaplin';
@@ -18,21 +18,84 @@ app.get('/', (req, res) => {
     res.send(myName + " " + today)
 })
 
-app.get('/list-movies', (req, res) => {
-    let movieString = favoriteMovieList.join(',')
-    res.send(movieString)
+// movie CRUD
+
+const favoriteMovieList = ['The Matrix', 'The Other Guys', 'The Shawshank Redemption']
+
+// Create
+
+app.post('/new-movie', (req, res) => {
+    favoriteMovieList.push(req.body.title)
+    res.json(favoriteMovieList)
 })
 
-app.get('/single-movie/:movieName', (req, res)=>{
-    const foundMovie = favoriteMovieList.find((movie)=> {
-        return movieName === req.params.movieName
-    })
-    if (foundMovie) {
-        res.send(foundMovie)
-    } else {
-        res.send('no movie found')
-    }
+// Read Movies
+app.get("/all-movies", (req, res)=> {
+    res.json(favoriteMovieList)
 })
+
+
+// Update
+
+app.put("/update-movie/:titleToUpdate", (req, res) => {
+    //find movie and update title
+    const titleToUpdate = req.params.titleToUpdate
+    const newTitle = req.body.newTitle
+
+    const indexOfMovie = favoriteMovieList.indexOf(titleToUpdate)
+
+    if (indexOfMovie < 0) {
+        res.json('movie not found')
+        return;
+    }
+    
+    favoriteMovieList[indexOfMovie] = newTitle
+    res.json({
+        success: true
+    })
+})
+
+// Delete
+
+app.delete("/delete-movie/:titleToDelete", (req, res) => {
+    const titleToDelete = req.params.titleToDelete
+    const indexOfTitle = favoriteMovieList.indexOf(titleToDelete)
+    if (indexOfTitle < 0) {
+        res.json('movie not found')
+        return;
+    }
+    favoriteMovieList.splice(indexOfTitle, 1)
+
+    res.json({
+        "success": true
+    })
+})
+
+// app.get('/list-movies', (req, res) => {
+//     let movieString = favoriteMovieList.join(',')
+//     res.send(movieString)
+// })
+
+// app.get('/single-movie/:movieName', (req, res)=>{
+//     const foundMovie = favoriteMovieList.find((movie)=> {
+//         return movieName === req.params.movieName
+//     })
+//     if (foundMovie) {
+//         res.send(foundMovie)
+//     } else {
+//         res.send('no movie found')
+//     }
+// })
+
+
+
+// app.get('/add-movie', (req, res) => {
+//     let newMovie = req.query.newMovie
+//     favoriteMovieList.push(newMovie)
+//     console.log(favoriteMovieList)
+// })
+
+// USER CRUD
 
 app.post('/save-user-info', (req, res) => {
     const queryParamFirstName = req.body.firstName
@@ -44,11 +107,6 @@ app.get('/show-user-info', (req, res) => {
     res.send()
 })
 
-app.get('/add-movie', (req, res) => {
-    let newMovie = req.query.newMovie
-    favoriteMovieList.push(newMovie)
-    console.log(favoriteMovieList)
-})
 
 app.listen(port, ()=> {
     console.log('listening on port: ' + port)
